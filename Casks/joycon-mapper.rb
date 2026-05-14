f352f8d7f65aacfec4330948aeb7e893b2cdf8d2
Naoki Muramoto <foxhound@FOXHOUNDPro14-M1MAX.local>
2026-05-14 22:06:28 +0900

    Update Joycon Mapper to 0.5.0

--- Casks/joycon-mapper.rb
+++ Casks/joycon-mapper.rb
@@ -1,15 +1,15 @@
 cask "joycon-mapper" do
-  version "0.1.0"
-  sha256 "742fae045014fb26077901eefd73ded9d0752f0b7a303c5c1d2f9eff8dd894fc"
+  version "0.5.0"
+  sha256 "a1286db36c68af0a4eaaf52147a07f54068b0e7cd1dde2735434bd4591a43f13"
 
   url "https://github.com/naoki-mrmt/joycon-mapper/releases/download/v#{version}/JoyconMapper-v#{version}.zip"
   name "Joycon Mapper"
-  desc "Use a Nintendo Switch Joy-Con (L) as a macOS pointer and shortcut controller"
+  desc "Use a Nintendo Switch Joy-Con (L) as a pointer and shortcut controller"
   homepage "https://github.com/naoki-mrmt/joycon-mapper"
+
+  depends_on macos: ">= :tahoe"
 
   app "JoyconMapper.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.muramoto-co.JoyconMapper.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.muramoto-co.JoyconMapper.plist"
 end
